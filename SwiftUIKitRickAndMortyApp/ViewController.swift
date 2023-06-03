985386dac131c4cb043bd1b177725f3694c06a99
Emre Yıldız <ey56748@gmail.com>
2023-06-03 23:08:58 +0300

    Initial commit

---
+++ SwiftUIKitRickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIKitRickAndMortyApp
+//
+//  Created by Emre Yıldız on 3.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
