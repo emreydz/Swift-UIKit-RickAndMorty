aca980c956e0bbb617aa3933195421f1e168c46c
Emre Yıldız <ey56748@gmail.com>
2023-06-05 23:07:47 +0300

    tabbar setup complate

--- SwiftUIKitRickAndMortyApp/Controllers/Core/RMTabViewController.swift
+++ SwiftUIKitRickAndMortyApp/Controllers/Core/RMTabViewController.swift
@@ -22,8 +22,13 @@ class RMTabViewController: UITabBarController {
         let epsiodeVC = RMEpsiodeViewController()
         let settingVC = RMSettingsViewController()
         
+        charaterVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationVC.navigationItem.largeTitleDisplayMode = .automatic
+        epsiodeVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingVC.navigationItem.largeTitleDisplayMode = .automatic
+        
         charaterVC.title = "Characters"
-        locationVC.title = "Loactions"
+        locationVC.title = "Location"
         epsiodeVC.title = "Epsiode"
         settingVC.title = "Settings"
         
@@ -31,6 +36,19 @@ class RMTabViewController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: locationVC)
         let nav3 = UINavigationController(rootViewController: epsiodeVC)
         let nav4 = UINavigationController(rootViewController: settingVC)
+        
+        nav1.tabBarItem = UITabBarItem( title: "Characters", image: UIImage(systemName: "person"), tag: 1
+        )
+        nav2.tabBarItem = UITabBarItem( title: "Location", image: UIImage(systemName: "globe"), tag: 2
+        )
+        nav3.tabBarItem = UITabBarItem( title: "Epsiode", image:  UIImage(systemName: "tv"), tag: 3
+        )
+        nav4.tabBarItem = UITabBarItem( title: "Settings", image:  UIImage(systemName: "gear"), tag: 4
+        )
+        
+        for nav in [nav1,nav2,nav3,nav4]{
+            nav.navigationBar.prefersLargeTitles = true
+        }
         
         
         setViewControllers([nav1,nav2,nav3,nav4], animated: true)
