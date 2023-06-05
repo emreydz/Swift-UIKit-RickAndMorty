97f63715965a71bbb4ffc248336d040b631ef7b0
Emre Yıldız <ey56748@gmail.com>
2023-06-05 22:56:19 +0300

    tabview setup

--- SwiftUIKitRickAndMortyApp/Controllers/Core/RMTabViewController.swift
+++ SwiftUIKitRickAndMortyApp/Controllers/Core/RMTabViewController.swift
@@ -12,10 +12,28 @@ class RMTabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setUpTabps()
         
     }
 
-
+    private func setUpTabps(){
+        let charaterVC = RMCharacterViewController()
+        let locationVC = RMLocationViewController()
+        let epsiodeVC = RMEpsiodeViewController()
+        let settingVC = RMSettingsViewController()
+        
+        charaterVC.title = "Characters"
+        locationVC.title = "Loactions"
+        epsiodeVC.title = "Epsiode"
+        settingVC.title = "Settings"
+        
+        let nav1 = UINavigationController(rootViewController: charaterVC)
+        let nav2 = UINavigationController(rootViewController: locationVC)
+        let nav3 = UINavigationController(rootViewController: epsiodeVC)
+        let nav4 = UINavigationController(rootViewController: settingVC)
+        
+        
+        setViewControllers([nav1,nav2,nav3,nav4], animated: true)
+    }
 }
 
