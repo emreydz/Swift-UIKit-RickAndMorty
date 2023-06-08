48131726ea20c022fc48c5754f57ce2e9f63f52e
Emre Yıldız <ey56748@gmail.com>
2023-06-08 22:46:05 +0300

    service

--- SwiftUIKitRickAndMortyApp/Controllers/Core/RMCharacterViewController.swift
+++ SwiftUIKitRickAndMortyApp/Controllers/Core/RMCharacterViewController.swift
@@ -11,8 +11,17 @@ final class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
- 
+       
+        RMService.shared.execute(.listCharactersRequests, expecting: RMCharacter.self){ result in
+            switch result {
+            case .success(let model):
+                print(String(describing: model))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+            
+        }
+        
     }
 }
---
+++ SwiftUIKitRickAndMortyApp/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
@@ -0,0 +1,8 @@
+//
+//  RMGetAllCharactersResponse.swift
+//  SwiftUIKitRickAndMortyApp
+//
+//  Created by Emre Yıldız on 8.06.2023.
+//
+
+import Foundation
